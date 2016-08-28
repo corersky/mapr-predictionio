69bd5579cb739498a49b908bee98d08ed1a6811f
Dong Meng <dmeng@Dmeng-MB13.local>
2016-08-28 16:26:17 -0700

    travis

--- data/src/main/scala/org/apache/predictionio/data/storage/hbase/HBPEvents.scala
+++ data/src/main/scala/org/apache/predictionio/data/storage/hbase/HBPEvents.scala
@@ -24,7 +24,8 @@ import org.apache.predictionio.data.storage.StorageClientConfig
 import org.apache.hadoop.hbase.client._
 import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
-import org.apache.hadoop.hbase.mapreduce.{IdentityTableMapper, TableMapReduceUtil}
+import org.apache.hadoop.hbase.mapreduce.IdentityTableMapper
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil.initTableMapperJob
 import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat
 import org.apache.hadoop.hbase.mapreduce.TableOutputFormat
@@ -107,7 +108,7 @@ class HBPEvents(client: HBClient, config: StorageClientConfig, namespace: String
     val conf = HBaseConfiguration.create()
     conf.set(TableInputFormat.INPUT_TABLE, table)
     val job = Job.getInstance(conf)
-    TableMapReduceUtil.initTableMapperJob(table, scan, classOf[IdentityTableMapper], null, null, job)
+    initTableMapperJob(table, scan, classOf[IdentityTableMapper], null, null, job)
     job.getConfiguration
   }
 
